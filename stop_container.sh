f64c93b45771006f85ecd809f4f78f89dd02a1b6
gmanne <132149493+gmanne11@users.noreply.github.com>
2024-05-28 04:07:38 +0530

    Update stop_container.sh

--- stop_container.sh
+++ stop_container.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
-# Stop the running containers
-containerid=`docker ps | awk -F " " '{print$1}'`
-docker rm -f "$containerid"
+# Stop and remove the running containers
+docker ps -q | while read -r containerid; do
+    echo "Stopping and removing container $containerid"
+    docker rm -f "$containerid"
+done
