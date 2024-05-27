08e053577039fde111da42bf776d4ea3610cc1e4
gmanne11 <gopim4959@gmail.com>
2024-05-28 03:32:05 +0530

    Typo in start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the image from Dockerhub
-dockr pull vivekmanne/simple-python-flask-app:latest 
+docker pull vivekmanne/simple-python-flask-app:latest 
 
 # Run the image as a container
 docker run -d -p 5000:5000 vivekmanne/simple-python-flask-app:latest
